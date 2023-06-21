3190d1c37299e9b51c9293584ad21f3b8d0c8fbe
SandroFernandes <sandro.fernandes@softrent.com.br>
2023-06-21 08:59:00 -0300

    Is_prime_re.py

---
+++ Prime numbers/Is_prime_re.py
@@ -0,0 +1,12 @@
+# Check if a number is prime or not using regular expression
+
+import re
+
+
+def is_prime_re(n):
+    return re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None
+
+
+if __name__ == '__main__':
+    for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]:
+        print(f'{x} is {"prime " if is_prime_re(x) else "not prime"}')
